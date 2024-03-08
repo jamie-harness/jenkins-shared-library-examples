f24070b372a9b757d83480422ddbc088efa23582
Jamie Li <tianyi.li@harness.io>
2024-03-07 23:40:28 -0500

    Update runGoTests.groovy

--- vars/runGoTests.groovy
+++ vars/runGoTests.groovy
@@ -1,5 +1,5 @@
 def call() {
     sh "go install github.com/jstemmer/go-junit-report/v2@latest"
-    sh "go test -v 2>&1 -cover ./... | $HOME/go/bin/go-junit-report -set-exit-code > report.xml || true"
+    sh "go test -v 2>&1 -cover ./... | $HOME/go/bin/go-junit-report -set-exit-code > report.xml"
     junit "report.xml"
 }
